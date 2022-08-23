47c93f04eccd1ef72cdca7a3317e09e403516b1c
urmi mahapatra <urmi.mahapatra@gmail.com>
2022-08-24 00:08:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+
+
+app.listen(port, function(err){
+    if (err){
+        console.log(`Error in running the Server : ${err}`);
+    }
+    console.log(`Server is running on port : ${port}`);
+
+});
+
+console.log('hi');
